101d141d5b8206091309a87619bee9609d2251ef
sghatge203 <33164704+sghatge203@users.noreply.github.com>
2022-02-04 18:43:00 +0100

    added new ui

--- src/pages/payment.js
+++ src/pages/payment.js
@@ -18,7 +18,9 @@ import psplogo from "../psp.png";
 import { runProgressbar } from "../global/global";
 import audioCall from "../audio.mp3";
 import success from "../success.jpg";
-
+import npcilogo from "../npci.png";
+import screen from "../screen.png";
+import failIcon from '../fail.png'
 import axios from "axios";
 var requestOptions = {
   method: "POST",
@@ -66,6 +68,7 @@ const Payment = (props) => {
   var [move8, setMove8] = useState(0);
   const [isMobile, setIsMobile] = useState(false);
   var [transactionId, setTransactionId] = useState("");
+  const [countryCode, setCountryCode] = useState("+91");
   const handleChange = (evt) => {
     const { name, value } = evt.target;
     setFormData({
@@ -78,20 +81,20 @@ const Payment = (props) => {
     if (!validate()) {
       e.preventDefault();
       setIsLoader(true);
-      data.ReqPay.Payer.Device.Tag[0].value = formData.mobile;
+      data.ReqPay.Payer.Device.Tag[0].value = countryCode + formData.mobile;
       data.ReqPay.Payer.Amount.value = formData.amount;
       data.ReqPay.Payer.addr = formData.mobile + "@" + formData.bankId;
       data.ReqPay.Payer.name = formData.mobile;
       requestOptions.body = JSON.stringify(data);
-
+      console.log("formD", data);
       fetch(
         "https://et32mpbgpe.execute-api.us-east-1.amazonaws.com/Dev/payment",
         requestOptions
       )
         .then((res) => res.clone().json())
         .then((res) => {
-          localStorage.setItem("TXID",res.body)
-            setTransactionId(res.body);
+          localStorage.setItem("TXID", res.body);
+          setTransactionId(res.body);
           console.log("PaymentResponse", res);
         })
         .catch((err) => {
@@ -302,7 +305,7 @@ const Payment = (props) => {
   }
 
   const getStatus = async () => {
-    console.log('transactionId',transactionId)
+    console.log("transactionId", transactionId);
     let data = {
       "Transaction-ID": localStorage.getItem("TXID"),
     };
@@ -315,7 +318,7 @@ const Payment = (props) => {
       .then((res) => {
         setIsMobile(false);
         console.log("statusResponse", res);
-        clearData()
+        clearData();
         if (res.body == "true") {
           setIsSuccess(true);
           setIsFailure(false);
@@ -324,17 +327,125 @@ const Payment = (props) => {
         }
       })
       .catch((err) => {
-        clearData()
+        clearData();
         setIsMobile(false);
         setIsFailure(true);
         console.log("statusError", err);
       });
   };
+
+  const goback = ()=>{
+    setIsFailure(false)
+    clearData()
+    setIsSuccess(false)
+  }
   return (
     <>
       <div className="container-fluid">
         <div className={"row row-gutter"}>
-          <div className="col-md-3 mobile-view">
+          <div className="col-md-3">
+            <div className="smartphone">
+              <div className="content">
+                <div className="phone-header">
+                  <div className="text-center mart-title">Minimart</div>
+                </div>
+                <div className="phone-logo">
+                  <div className="logo-card">
+                    <img src={logo} className="height-30" alt="logo" />
+                  </div>
+                </div>
+                <div className="phone-content">
+                  <div className="form-content">
+                    <div className="form-group">
+                      <div className="enter-amount">
+                        <div className="ruppe-symbol">₹</div>
+                        <input
+                          name="amount"
+                          value={formData.amount}
+                          onChange={handleChange}
+                          type="text"
+                          className="form-control text-amount"
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group mt-3">
+                      <div className="mobile-form">
+                        <input
+                          value={countryCode}
+                          disabled
+                          className="form-control w-26 text-amount"
+                        />
+                        <input
+                          value={formData.mobile}
+                          name="mobile"
+                          onChange={handleChange}
+                          placeholder="Mobile..."
+                          type="text"
+                          required
+                          className="form-control w-66 text-box"
+                        />
+                      </div>
+                      <div className="at-sign">@</div>
+                    </div>
+                    <div className="form-group">
+                      <div className="upi-form">
+                        <select
+                          id="bankId"
+                          value={formData.bankId}
+                          name="bankId"
+                          type="select"
+                          onChange={handleChange}
+                          required
+                          className="form-control form-select text-box"
+                        >
+                          <option value={""}>Select</option>
+                          <option value={"icici"}>icici</option>
+                          <option value={"ybl"}>ybl</option>
+                          <option value={"okhdfcbank"}>okhdfcbank</option>
+                          <option value={"okaxis"}>okaxis</option>
+                          <option value={"oksbi"}>oksbi</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <div className="make-payment" onClick={handlePayment}>
+                    Request money
+                  </div>
+                </div>
+              </div>
+              {isLoader ? (
+              <div className="loader-bg">
+                <div className="loading">
+                  <img src={loader} alt="loading" className="height-130"/>
+                  <div>Payment Processsing</div>
+                </div>
+              </div>
+            ) : null}
+            {isSuccess ? (
+              <div className="loader-bg">
+                <div className="loading">
+                  <img src={success} alt="success" className="height-190" />
+                  <div className="payment-title">Payment Successfull</div>
+                  <div className="go-back-button-new" onClick={goback}>Make New Payment</div>
+
+                </div>
+              </div>
+            ) : null}
+             {isFailure ? (
+              <div className="loader-bg">
+                <div className="loading">
+                  <img src={failIcon} alt="success" className="height-190" />
+                  <div className="payment-title-fail">Payment Failure</div>
+                  <div className="go-back-button" onClick={goback}>Retry</div>
+                </div>
+              </div>
+            ) : null}
+            </div>
+          </div>
+          {/* <div className="col-md-3 mobile-view">
             {isLoader ? (
               <div className="loader-bg">
                 <div className="loading">
@@ -351,11 +462,6 @@ const Payment = (props) => {
                 </div>
               </div>
             ) : null}
-            {/* {isSuccess ? (
-              <Alert className="mt-3" color="success">
-                Payment Successfull
-              </Alert>
-            ) : null} */}
             {isFailure ? (
               <Alert className="mt-3" color="danger">
                 Payment Failure !!
@@ -424,7 +530,7 @@ const Payment = (props) => {
                 Request Money
               </button>
             </div>
-          </div>
+          </div> */}
 
           <div className="col-md-7">
             <div className="m-t-18">
@@ -435,8 +541,8 @@ const Payment = (props) => {
                   <Progress value={move8} className="justify-content-end" />
                 </div>
                 <div className="col-md-1">
-                  <i class="fas fa-university font-3em"></i>
-                  <div>PSP</div>
+                  <i class="fas fa-university font-3em psp-color"></i>
+                  <div className="psp-text">Aquairer/PSP</div>
                 </div>
                 <div className="col-md-2">
                   <Progress value={move2} />
@@ -444,8 +550,9 @@ const Payment = (props) => {
                   <Progress value={move7} className="justify-content-end" />
                 </div>
                 <div className="col-md-1">
-                  <i class="fas fa-university font-3em"></i>
-                  <div>NPIC</div>
+                  <div className="pt-3 mleft-12">
+                    <img src={npcilogo} className="height-30" alt="npcilogo" />
+                  </div>
                 </div>
                 <div className="col-md-2">
                   <Progress value={move3} />
@@ -453,8 +560,8 @@ const Payment = (props) => {
                   <Progress value={move6} className="justify-content-end" />
                 </div>
                 <div className="col-md-1">
-                  <i class="fas fa-university font-3em"></i>
-                  <div>PSP</div>
+                  <i class="fas fa-university font-3em issuer-color"></i>
+                  <div className="psp-text">Issuer/PSP</div>
                 </div>
                 <div className="col-md-3">
                   <Progress value={move4} /> <br />
@@ -464,7 +571,68 @@ const Payment = (props) => {
             </div>
           </div>
           <div className="col-md-2">
-            <div className="small-mobile">
+            <div class="telephone">
+              <div class="content">
+                <div className="screen">
+                  <img src={screen} alt="screen" className="width-100 " />
+                </div>
+                <div className="buttons">
+                  <button
+                    className="btn numric-btn green-btn"
+                    onClick={clickKeyboard}
+                  >
+                    <i className="fas fa-phone"></i>
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    Ok
+                  </button>
+                  <button
+                    className="btn numric-btn red-btn"
+                    onClick={clickKeyboard}
+                  >
+                    <i className="fas fa-phone-slash"></i>
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    1
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    2
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    3
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    4
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    5
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    6
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    7
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    8
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    9
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    *
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    0
+                  </button>
+                  <button className="btn numric-btn" onClick={clickKeyboard}>
+                    #
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="small-mobile">
               <div className="phone-screen">Mobile Phone</div>
               <div className="buttons">
                 <button className="btn numric-btn" onClick={clickKeyboard}>
@@ -504,7 +672,7 @@ const Payment = (props) => {
                   #
                 </button>
               </div>
-            </div>
+            </div> */}
             {isMobile ? (
               <div className="text-center">
                 <i class="fas fa-circle-notch fa-spin spinner"></i>
